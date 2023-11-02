4c09d3bc3d74f47d3c9db84742205633894bd029
Yahor Levanenka <witaway33@gmail.com>
2023-11-03 01:57:04 +0300

    Added empty users controller

---
+++ dumbogram/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using dumbogram.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dumbogram.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly ILogger<UsersController> logger;
+    private readonly ApplicationDbContext dbContext;
+    
+    public UsersController(ILogger<UsersController> logger, ApplicationDbContext dbContext)
+    {
+        this.logger = logger;
+        this.dbContext = dbContext;
+    }
+}
