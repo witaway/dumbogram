1830076be726696b821d581dd57869b7d870ecbf
Yahor Levanenka <witaway33@gmail.com>
2023-11-03 09:05:45 +0300

    Added BASIC AND TEMPORARY initialization exceptions handler

--- dumbogram/Program.cs
+++ dumbogram/Program.cs
@@ -2,16 +2,33 @@ namespace Dumbogram;
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.RegisterApplicationServices(builder.Configuration);
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.RegisterApplicationServices(builder.Configuration);
 
-        var app = builder.Build();
+            var app = builder.Build();
+            
+            app.ConfigureMiddleware();
+            app.RegisterEndpoints();
 
-        app.ConfigureMiddleware();
-        app.RegisterEndpoints();
+            app.Run();
+        }
+        catch (Exception exception)
+        {
+            // TEMPORARY SOLUTION!!
+            // Todo: Change WriteLine to normal logger
+            // How to? Logger initializes after app.Build(),
+            //    but errors might be occured later during WebApplicationBuilder configuration
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[FATAL] Error has occured during initialization");
+            Console.ResetColor();
+            Console.WriteLine(exception);
+            return exception.HResult;
+        }
 
-        app.Run();
+        return 0;
     }
 }
