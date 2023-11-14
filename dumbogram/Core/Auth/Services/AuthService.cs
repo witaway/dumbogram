d9127627cc3f4f9d92fe6fb7b7c446671f6e76c9
Yahor Levanenka <witaway33@gmail.com>
2023-11-15 00:33:04 +0300

    Send UserId in claims instead of Username

--- dumbogram/Core/Auth/Services/AuthService.cs
+++ dumbogram/Core/Auth/Services/AuthService.cs
@@ -1,5 +1,7 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Dumbogram.Core.Auth.Controllers;
+using Dumbogram.Database.Identity;
 using Microsoft.AspNetCore.Identity;
 
 namespace Dumbogram.Core.Auth.Services;
@@ -10,10 +12,10 @@ public class AuthService
     private readonly ILogger<AuthController> _logger;
     private readonly RoleManager<IdentityRole> _roleManager;
     private readonly TokenService _tokenService;
-    private readonly UserManager<IdentityUser> _userManager;
+    private readonly UserManager<ApplicationIdentityUser> _userManager;
 
     public AuthService(
-        UserManager<IdentityUser> userManager,
+        UserManager<ApplicationIdentityUser> userManager,
         RoleManager<IdentityRole> roleManager,
         IConfiguration configuration,
         ILogger<AuthController> logger,
@@ -27,17 +29,17 @@ public class AuthService
         _tokenService = tokenService;
     }
 
-    public async Task<bool> CheckPasswordCorrectness(IdentityUser user, string password)
+    public async Task<bool> CheckPasswordCorrectness(ApplicationIdentityUser user, string password)
     {
         return await _userManager.CheckPasswordAsync(user, password);
     }
 
-    private async Task<IEnumerable<Claim>> GetUserClaims(IdentityUser user)
+    private async Task<IEnumerable<Claim>> GetUserClaims(ApplicationIdentityUser user)
     {
         var userRoles = await _userManager.GetRolesAsync(user);
 
         var claims = new List<Claim>()
-            .Append(new Claim(ClaimTypes.Name, user.UserName))
+            .Append(new Claim(ClaimTypes.NameIdentifier, user.Id))
             .Append(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()))
             .Concat(
                 userRoles.Select(role => new Claim(ClaimTypes.Role, role))
@@ -46,14 +48,14 @@ public class AuthService
         return claims;
     }
 
-    public async Task<JwtSecurityToken> SignIn(IdentityUser user)
+    public async Task<JwtSecurityToken> SignIn(ApplicationIdentityUser user)
     {
         var claims = await GetUserClaims(user);
         var token = _tokenService.CreateJwtSecurityToken(claims);
         return token;
     }
 
-    public async Task<IdentityResult> SignUp(IdentityUser user, string password)
+    public async Task<IdentityResult> SignUp(ApplicationIdentityUser user, string password)
     {
         var result = await _userManager.CreateAsync(user, password);
         return result;
