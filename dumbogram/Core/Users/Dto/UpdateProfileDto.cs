60071802e4128f7ce1f222b4efd4cf8c1adf8c84
Yahor Levanenka <witaway33@gmail.com>
2023-11-15 00:35:14 +0300

    Removed AvatarMediaId (does not exist) and Name (should be set automatically) from UpdateProfileDto

--- dumbogram/Core/Users/Dto/UpdateProfileDto.cs
+++ dumbogram/Core/Users/Dto/UpdateProfileDto.cs
@@ -4,20 +4,18 @@ namespace Dumbogram.Core.Users.Dto;
 
 public class UpdateProfileDto
 {
-    public string? Name { get; set; }
+    // public string? Name { get; set; }
 
     public string? Description { get; set; }
-
-    public Guid? AvatarMediaId { get; set; }
 }
 
 public class UpdateProfileDtoValidator : AbstractValidator<UpdateProfileDto>
 {
     public UpdateProfileDtoValidator()
     {
-        // Name is null or Name.Length in [1; 255]
-        RuleFor(profile => profile.Name).Length(1, 255)
-            .When(profile => profile.Name != null);
+        // // Name is null or Name.Length in [1; 255]
+        // RuleFor(profile => profile.Name).Length(1, 255)
+        //     .When(profile => profile.Name != null);
 
         // Description is null or Description.Length in [1; 255]
         RuleFor(profile => profile.Description).Length(1, 255)
