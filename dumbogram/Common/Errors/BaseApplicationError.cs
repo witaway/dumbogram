c22c856c449b28c0385ebb30e86702e43e56a230
Yahor Levanenka <witaway33@gmail.com>
2023-11-17 03:32:36 +0300

    Custom Error type that always contains ErrorCode

---
+++ dumbogram/Common/Errors/BaseApplicationError.cs
@@ -0,0 +1,39 @@
+﻿using FluentResults;
+
+namespace Dumbogram.Common.Errors;
+
+public class BaseApplicationError : Error
+{
+    public BaseApplicationError(string errorCode, string message)
+        : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+
+    public BaseApplicationError(string errorCode, string message, BaseApplicationError causedBy)
+        : base(message, causedBy)
+    {
+        ErrorCode = errorCode;
+    }
+
+    public string ErrorCode
+    {
+        get => GetErrorCode();
+        private set => SetErrorCode(value);
+    }
+
+    private void SetErrorCode(string errorCode)
+    {
+        Metadata.Add("ErrorCode", errorCode);
+    }
+
+    private string GetErrorCode()
+    {
+        if (Metadata.TryGetValue("ErrorCode", out var errorCode))
+        {
+            return errorCode as string;
+        }
+
+        return "";
+    }
+}
