48f75b176f616bb988ca17f0bfd96e28c73ccdeb
Yahor Levanenka <witaway33@gmail.com>
2023-11-17 03:33:05 +0300

    Errors dedicated to Auth logic

---
+++ dumbogram/Core/Auth/Errors/AuthErrors.cs
@@ -0,0 +1,44 @@
+﻿using Dumbogram.Common.Errors;
+using Microsoft.AspNetCore.Identity;
+
+namespace Dumbogram.Core.Auth.Errors;
+
+public class PasswordNotValidError : BaseApplicationError
+{
+    public PasswordNotValidError(string message)
+        : base("PasswordNotValid", message)
+    {
+    }
+}
+
+public abstract class CredentialsConflictError : BaseApplicationError
+{
+    protected CredentialsConflictError(string errorCode, string message)
+        : base(errorCode, message)
+    {
+    }
+}
+
+public class UsernameAlreadyTakenError : BaseApplicationError
+{
+    public UsernameAlreadyTakenError(string message)
+        : base("UsernameAlreadyTaken", message)
+    {
+    }
+}
+
+public class EmailAlreadyTakenError : BaseApplicationError
+{
+    public EmailAlreadyTakenError(string message)
+        : base("EmailAlreadyTaken", message)
+    {
+    }
+}
+
+public class WrappedIdentityError : BaseApplicationError
+{
+    public WrappedIdentityError(IdentityError error)
+        : base(error.Code, error.Description)
+    {
+    }
+}
