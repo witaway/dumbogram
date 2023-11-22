fd1aa9b1588b157c494e60eba3ae893d0727fc5b
Yahor Levanenka <witaway33@gmail.com>
2023-11-22 06:21:15 +0300

    Added controller with chats of current user

---
+++ dumbogram/Core/Chats/Controllers/MyChatsController.cs
@@ -0,0 +1,59 @@
+﻿using Dumbogram.Common.Dto;
+using Dumbogram.Common.Extensions;
+using Dumbogram.Core.Chats.Dto;
+using Dumbogram.Core.Chats.Services;
+using Dumbogram.Core.Users.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dumbogram.Core.Chats.Controllers;
+
+[Authorize]
+[Route("/api/users/me/chats")]
+[ApiController]
+public class MyChatsController : ControllerBase
+{
+    private readonly ChatService _chatService;
+
+    private readonly ILogger<ChatsController> _logger;
+    private readonly UserService _userService;
+
+    public MyChatsController(
+        ChatService chatService,
+        UserService userService,
+        ILogger<ChatsController> logger
+    )
+    {
+        _chatService = chatService;
+        _userService = userService;
+        _logger = logger;
+    }
+
+    [ProducesResponseType(StatusCodes.Status200OK,
+        Type = typeof(ResponseSuccess<ReadMultipleChatsShortInfoResponseDto>)
+    )]
+    [HttpGet("owned")]
+    public async Task<IActionResult> ReadOwnedChats()
+    {
+        var userProfile = await _userService.ReadUserProfileById(User.GetUserApplicationId());
+
+        var chats = await _chatService.ReadAllChatsOwnedBy(userProfile!);
+
+        var chatsDto = new ReadMultipleChatsShortInfoResponseDto(chats);
+        return Ok(Common.Dto.Response.Success("Chats list accessed successfully", chatsDto));
+    }
+
+    [ProducesResponseType(StatusCodes.Status200OK,
+        Type = typeof(ResponseSuccess<ReadMultipleChatsShortInfoResponseDto>)
+    )]
+    [HttpGet("joined")]
+    public async Task<IActionResult> ReadJoinedChats()
+    {
+        var userProfile = await _userService.ReadUserProfileById(User.GetUserApplicationId());
+
+        var chats = await _chatService.ReadAllChatsJoinedBy(userProfile!);
+
+        var chatsDto = new ReadMultipleChatsShortInfoResponseDto(chats);
+        return Ok(Common.Dto.Response.Success("Chats list accessed successfully", chatsDto));
+    }
+}
