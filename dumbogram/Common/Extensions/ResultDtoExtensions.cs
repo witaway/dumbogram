f25f293b4d697bc951640965d27549a54f623705
Yahor Levanenka <witaway33@gmail.com>
2023-11-22 06:19:03 +0300

    Improved ToFailureDto, now it works with generic Result<T> too

--- dumbogram/Common/Extensions/ResultDtoExtensions.cs
+++ dumbogram/Common/Extensions/ResultDtoExtensions.cs
@@ -27,4 +27,26 @@ public static class ResultDtoExtensions
 
         return Response.Failure(message, errorDtos);
     }
+
+    public static Response ToFailureDto<T>(this Result<T> result, string message)
+    {
+        if (result.IsSuccess)
+        {
+            throw new Exception("ToFailureDto cannot be called when IsSuccess=true");
+        }
+
+        var errors = result.Errors;
+        var errorDtos = errors.Select(error =>
+        {
+            var applicationError = error as BaseApplicationError;
+            if (applicationError != null)
+            {
+                return new ErrorDto(applicationError);
+            }
+
+            return new ErrorDto(error);
+        });
+
+        return Response.Failure(message, errorDtos);
+    }
 }
