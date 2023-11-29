eb64a60244e30d7725c557c43362e5f6a936bb30
Yahor Levanenka <witaway33@gmail.com>
2023-11-29 18:22:18 +0300

    Service to send system messages

---
+++ dumbogram/Application/Chats/Services/SystemMessagesService.cs
@@ -0,0 +1,66 @@
+﻿using Dumbogram.Database;
+using Dumbogram.Models.Chats;
+using Dumbogram.Models.Messages.SystemMessages;
+using Dumbogram.Models.Users;
+
+namespace Dumbogram.Application.Chats.Services;
+
+public class SystemMessagesService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public SystemMessagesService(
+        ApplicationDbContext dbContext
+    )
+    {
+        _dbContext = dbContext;
+    }
+
+    private async Task EnsureSystemMessageCreated(SystemMessage systemMessage)
+    {
+        await _dbContext.AddAsync(systemMessage);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task CreateJoinedMessage(Chat chat, UserProfile joinedUser)
+    {
+        var message = new JoinedSystemMessage
+        {
+            Chat = chat,
+            SubjectProfile = joinedUser
+        };
+        await EnsureSystemMessageCreated(message);
+    }
+
+    public async Task CreateLeftMessage(Chat chat, UserProfile leftUser)
+    {
+        var message = new LeftSystemMessage
+        {
+            Chat = chat,
+            SubjectProfile = leftUser
+        };
+        await EnsureSystemMessageCreated(message);
+    }
+
+    public async Task CreateEditedTitleMessage(Chat chat, UserProfile subject, string newTitle)
+    {
+        var message = new EditedTitleSystemMessage
+        {
+            Chat = chat,
+            SubjectProfile = subject,
+            NewContent = newTitle
+        };
+        await EnsureSystemMessageCreated(message);
+    }
+
+    public async Task CreateEditedDescriptionMessage(Chat chat, UserProfile subject, string newDescription)
+    {
+        var message = new EditedDescriptionSystemMessage
+        {
+            Chat = chat,
+            SubjectProfile = subject,
+            NewContent = newDescription
+        };
+        await EnsureSystemMessageCreated(message);
+    }
+}
