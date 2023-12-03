038735cfa8b25a5132ea373eb4cd790d5944dcc0
Yahor Levanenka <witaway33@gmail.com>
2023-12-03 05:05:28 +0300

    Added file formats and utilities to retrieve extensions, mime types and signatures of them

---
+++ dumbogram/Application/Files/Services/FileFormats/FileFormatGroups.cs
@@ -0,0 +1,17 @@
+﻿namespace Dumbogram.Application.Files.Services.FileFormats;
+
+public static class FileFormatGroups
+{
+    public static readonly List<FileFormat> Photo = new()
+    {
+        FileFormat.Jpeg,
+        FileFormat.Png,
+        FileFormat.Jpeg2000,
+        FileFormat.Jpg
+    };
+
+    public static readonly List<FileFormat> Audio = new()
+    {
+        FileFormat.Mp3
+    };
+}
